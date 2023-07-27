1ac34eb832ca695e81d2bb181141b3ef1752729d
mitali791 <75540008+mitali791@users.noreply.github.com>
2023-07-28 00:44:25 +0530

    Delete src directory

--- src/main/java/com/shortener/repository/ConfigurationsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shortener.repository;
-
-import com.shortener.model.UrlConfigurations;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConfigurationsRepository extends JpaRepository<UrlConfigurations, Long> {
-    UrlConfigurations findByConfigKey(String configKey);
-}
