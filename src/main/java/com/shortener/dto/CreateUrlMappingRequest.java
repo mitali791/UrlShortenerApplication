65cbe1f4d88c03fca936800ae8252c40de71c268
mitali791 <75540008+mitali791@users.noreply.github.com>
2023-07-28 00:41:51 +0530

    Add files via upload

---
+++ src/main/java/com/shortener/dto/CreateUrlMappingRequest.java
@@ -0,0 +1,19 @@
+package com.shortener.dto;
+
+import java.util.Date;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateUrlMappingRequest {
+
+    @NotNull
+    private String longUrl;
+
+    private Date expiryTime;
+
+
+}
